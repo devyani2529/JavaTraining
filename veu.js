4029477e982a3eb8ba1722c3b6329a0767e0ff6a
Devyani sonawane <73746623+devyani2529@users.noreply.github.com>
2023-08-07 18:03:42 +0530

    Update veu.js

--- veu.js
+++ veu.js
@@ -1,3 +1,5 @@
 
-GitHhttps://raybo.org/slides_vue3/notes/slides/02_02%20Computed%20Content/
-ub - LinkedInLearning/vue3-esst-2834032 
+GitHub - LinkedInLearning/vue3-esst-2834032 
+ 
+
+https://raybo.org/slides_vue3/notes/slides/02_02%20Computed%20Content/
